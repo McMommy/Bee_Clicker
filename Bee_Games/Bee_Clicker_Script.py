27a52468d7455b808b054a9aedc6a58023baab3d
McMommy <dylanmcquade1993@gmail.com>
2025-02-23 15:20:22 -0800

    set up a thread for the game to run in. the bees honey now goes up 1 each second and doesnt make everythign else pause. can now harvest what honey is made.

--- Bee_Games/Bee_Clicker_Script.py
+++ Bee_Games/Bee_Clicker_Script.py
@@ -1,43 +1,58 @@
 #make a system instead of it instatly givig you honey. it actually stores up and when you call harvest it picked up all the stored honey.
 
 import time
-import threading # use threading to make honey production happen in the background until harvest is called
+import threading # use threading to make honey production happen in the background until harvest is called ||||| threading.Join() waits for the thread to be done before going on.
 import random
 
-Bee = 0
-Honey = 0
-Command = ""
+bee = 0
+honey = 0
+bees_honey = 0
+command = ""
 
 
 
 
 def StartGame():
-    Actions()
+    passive_honey.start()
+    actions()
 
 
 
-#def Increase_Honey():
-   # global Honey
-   # Stored_Honey = 0
-   # if  
-   #     while Honey <= 10:
-    #        Stored_Honey += 1
-    #        time.sleep(2)
-   # elif
+def honey_production():
+    global honey
+    global command
+    global bees_honey
+    while command != "exit":
+        bees_honey += 1 # (additional_honey) * honey_multiplier}} bee_honey is var name
+        time.sleep(1)
+
+
+def sell_honey():
+    global honey
 
 
 
-def Actions():
-    global Honey
-    Command = ""
-    while Command != "exit":
-        Command = input("")
-        if Command == "harvest":
-            Increase_Honey()
-        #elif input == "":
-            #Choose_Bee()
-        elif Command == "exit":
+def actions():
+    global honey
+    global bees_honey
+    command = ""
+    while command != "exit":
+        command = input("")
+        if command == "harvest":
+            print(f"harvesting {bees_honey} honey from beehive")
+            honey += bees_honey
+            bees_honey = 0
+            print(f"you now have {honey} jars of honey")
+        elif input == "sell":
+            sell_honey()
+        elif command == "exit":
             print("Leaving The Farm!")
+
+
+
+
+#Threads
+passive_honey = threading.Thread(target=honey_production)
 
 
 
